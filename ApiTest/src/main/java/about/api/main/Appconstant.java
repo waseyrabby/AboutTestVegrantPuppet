63e1ad9bbbe89ef77f1c63c28422f8c0ce669fe8
Aam Wasey <waseyrabby@btinternet.com>
2016-10-18 22:50:57 -0400

    api is working but ui is not

---
+++ ApiTest/src/main/java/about/api/main/Appconstant.java
@@ -0,0 +1,17 @@
+package about.api.main;
+
+/**
+ * Created by wasey on 10/18/16.
+ */
+public class Appconstant
+
+
+
+{
+
+    public static final String link ="http://awesome.dev/fake-api-call.php";
+    public static final String accept ="application/json";
+
+
+
+}
